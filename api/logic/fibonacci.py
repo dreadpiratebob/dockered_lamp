a079afb4cef908d5cd38def1429dac16989005d9
dreadpiratebob <poseidon.guy@gmail.com>
2026-06-01 03:58:49 -0700

    calculating the nth fibonacci number for n <= 0.

--- api/logic/fibonacci.py
+++ api/logic/fibonacci.py
@@ -1,13 +1,18 @@
-from exceptions.http_base import BadRequestException
-
 def get_nth_fibonacci_number(n:int) -> int:
-  if n <= 0:
-    raise BadRequestException('an index in the Fibonacci sequence must be positive.')
-  
-  if n <= 2:
+  if n == 1 or n == 2:
     return 1
   
+  # this case maintains the property that the nth fibonacci number + the (n+1)th fibonnacci number = the (n+2)th fibonacci ... but it works for n <= 0.
+  if n < 1:
+    a, b, i = 1, 1, 0
+    
+    while i >= n:
+      a, b, i = b, a - b, i - 1
+    
+    return b
+  
   a, b = 1, 1
+  
   for i in range(n - 2):
     a, b = b, a + b
   
